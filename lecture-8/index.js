5a2278561c785029f9d352160288700feb4406af
Emrul Hassan <hemrul494@gmail.com>
2023-01-20 02:37:11 +0600

    second-commit

--- lecture-8/index.js
+++ lecture-8/index.js
@@ -3,3 +3,15 @@
 
 
 //prove: function is a value
+function testFunction(){
+    console.log('i am a test function');
+}
+const fn = testFunction;
+console.log(fn);
+fn();
+
+const arr= [fn,testFunction];
+
+const obj={
+    fn:testFunction
+}
